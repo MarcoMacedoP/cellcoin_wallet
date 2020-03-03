44511c6545726b274f9d1b510bf676f72302d732
Marco <marcosiegman01@gmail.com>
2020-03-03 13:41:47 -0600

    added header icons

--- src/screens/Balance/Router/options.tsx
+++ src/screens/Balance/Router/options.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import {StackNavigationOptions} from '@react-navigation/stack';
 import {colors} from 'shared/styles';
-import {LayoutHeader} from 'shared/components';
+import styled from 'styled-components/native';
+import {IconContainer} from 'shared/styled-components';
+import MIcon from 'react-native-vector-icons/Ionicons';
+import SimpleIcon from 'react-native-vector-icons/SimpleLineIcons';
 
-const balanceScreen: StackNavigationOptions = {
-  title: 'Wallet',
+const balanceScreen: any = ({navigation}) => ({
+  title: 'AgaveCoin',
   headerTitleAlign: 'center',
-  headerTintColor: colors.white,
   headerTitleStyle: {
     fontSize: 16,
     fontWeight: 'bold',
   },
   headerTransparent: true,
-  headerTitle: props => <LayoutHeader {...props} />,
-  headerBackTitleVisible: false,
-  headerLeft: null,
-};
-
+  headerBackTitleVisible: true,
+  headerLeftContainerStyle: {
+    padding: 16,
+  },
+  headerRightContainerStyle: {
+    padding: 16,
+  },
+  headerLeft: () => (
+    <IconContainer onPress={() => navigation.navigate('Notifications')}>
+      <MIcon name="ios-notifications-outline" size={24} color={colors.white} />
+    </IconContainer>
+  ),
+  headerRight: () => (
+    <IconContainer onPress={() => navigation.navigate('Balance')}>
+      <SimpleIcon name="wallet" size={18} color={colors.white} />
+    </IconContainer>
+  ),
+  headerTitle: () => <Logo source={require('assets/icons/logo_mini.png')} />,
+});
+const Logo = styled.Image`
+  margin: 0 auto;
+  width: 110px;
+  resize-mode: contain;
+`;
 export {balanceScreen};
--- src/screens/Balance/screens/BalanceScreen.tsx
+++ src/screens/Balance/screens/BalanceScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useLayoutEffect} from 'react';
 import styled from 'styled-components/native';
 import {RefreshControl, Dimensions} from 'react-native';
 import {BalanceHeaderComponent} from '../components/Header';
