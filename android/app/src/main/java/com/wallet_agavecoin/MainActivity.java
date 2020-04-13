ad9a52a8afb1ff09d9cd4ad97530f8f3fd0ab659
Fcasasherrera <dephermoure@hotmail.com>
2020-04-13 15:19:22 -0500

    Cambio de nombre de la app
    aun queda pendiente cambiar el nombre de ios

--- android/app/src/main/java/com/wallet_agavecoin/MainActivity.java
+++ android/app/src/main/java/com/wallet_agavecoin/MainActivity.java
@@ -1,4 +1,4 @@
-package com.wallet_template;
+package com.wallet_agavecoin;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "wallet_template";
+    return "wallet_agavecoin";
   }
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
